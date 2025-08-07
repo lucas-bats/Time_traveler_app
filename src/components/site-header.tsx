1c3ab5d6e37497175779122cd2b113533b632f3a
Lucas A P Batista <lucaspaesbatista1@gmail.com>
2025-08-07 14:40:40 +0000

    Ótimo! Agora, nas páginas das personalidades, coloque os botões que estã

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -15,15 +15,13 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <div className="flex gap-6 md:gap-10">
-          <Link href="/" className="flex items-center space-x-2">
-            <span className="hidden sm:inline-block font-bold font-headline text-primary text-xl">
+      <div className="container flex h-16 items-center">
+        <div className="flex flex-1 items-center gap-2 md:gap-4">
+          <Link href="/" className="flex items-center space-x-2 mr-4">
+            <span className="font-bold font-headline text-primary text-xl">
               {t.title}
             </span>
           </Link>
-        </div>
-        <div className="flex flex-1 items-center justify-end space-x-1">
           <nav className="flex items-center space-x-1">
             <Button variant="ghost" asChild>
               <Link href="/">
